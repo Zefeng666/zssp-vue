45b0fb1550d9dd24326e68bbb5679de3e2e08816
zefeng <245613360@qq.com>
2018-09-25 19:32:57 +0800

    修改bug

--- src/api/api.js
+++ src/api/api.js
@@ -3,6 +3,20 @@ import config from '../config/config'
 const request = config.request
 const API = {
 
+  // 登录
+  queryLogin (params) {
+    return request({
+      url: '/login',
+      params: params
+    })
+  },
+  // 后台首页数据
+  queryIndex (params) {
+    return request({
+      url: '/queryIndex',
+      params: params
+    })
+  },
   // 用户列表
   queryUser (params) {
     return request({
@@ -17,10 +31,38 @@ const API = {
       params: params
     })
   },
+  // 删除用户
+  deleteUser (params) {
+    return request({
+      url: '/deleteUser',
+      params: params
+    })
+  },
   // 发货申请
   queryOrder (params) {
     return request({
       url: '/queryOrder',
+      params: params
+    })
+  },
+  // 审核发货
+  auditOrder (params) {
+    return request({
+      url: '/auditOrder',
+      params: params
+    })
+  },
+  // 提现申请
+  queryWithdraw (params) {
+    return request({
+      url: '/queryWithdraw',
+      params: params
+    })
+  },
+  // 审核提现
+  auditWithdraw (params) {
+    return request({
+      url: '/auditWithdraw',
       params: params
     })
   }
